2bce72d45b1f73a1a459c65315ae46ad53e812d9
Ayan Majumdar <ayanmajumdar1992@gmail.com>
2016-12-28 11:09:49 +0530

    Update bwtinverse.py

--- bwtinverse.py
+++ bwtinverse.py
@@ -3,30 +3,30 @@ import sys
 
 def InverseBWT(bwt):
     # write your code here
-    list1 = list(bwt)
+    bwt_list = list(bwt)
     cnt_map = {}
-    map1 = {}
+    index_map = {}
     for i in range(len(bwt)):
     	if not bwt[i] in cnt_map:
     		cnt_map[bwt[i]] = 1
     	else:
     		cnt_map[bwt[i]] += 1
-    	list1[i] += str(cnt_map[bwt[i]])
-    	map1[list1[i]] = None
+    	bwt_list[i] += str(cnt_map[bwt[i]])
+    	index_map[bwt_list[i]] = None
     keys = list(cnt_map.keys())
     keys.sort()
     index = 0
     for i in keys:
     	count = cnt_map[i]
     	for j in range(count):
-    		map1[i+str(j+1)] = index
+    		index_map[i+str(j+1)] = index
     		index += 1
     rev = "$"
     i = 0
     while 1:
-    	rev += list1[i][0]
-    	i = map1[list1[i]]
-    	if list1[i][0] == '$':
+    	rev += bwt_list[i][0]
+    	i = index_map[bwt_list[i]]
+    	if bwt_list[i][0] == '$':
     		break
     return rev[::-1]
 
